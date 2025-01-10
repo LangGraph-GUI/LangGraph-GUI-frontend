a064a7561d147ae8989765da20a7f05f5cc6c79c
Homun Mage <homun@posetmage.com>
2025-01-10 16:25:15 +0800

    TODO: fix bugs....

--- src/redux/slices/subGraphSlice.js
+++ src/redux/slices/subGraphSlice.js
@@ -3,40 +3,50 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    rootGraph: null, // Initialize to null or an initial ID
-    subGraphs: [], // Array to hold subgraphs objects ({graphId: 1, nodes:[], edges:[]})
+    subGraphs: [{ 
+        graphName: "root", 
+        nodes: [],
+        node_counter: 1,
+    }],
 };
 
 const subGraphSlice = createSlice({
   name: 'subGraphs',
   initialState,
   reducers: {
-    setRootGraph: (state, action) => {
-      state.rootGraph = action.payload;
-    },
+    
     addSubGraph: (state, action) => {
-      state.subGraphs.push(action.payload);
+        const { graphName, nodes, node_counter } = action.payload;
+
+        // Check if a subgraph with the same name already exists
+        const graphExists = state.subGraphs.some(graph => graph.graphName === graphName);
+        if (graphExists) {
+            console.error(`Subgraph with name '${graphName}' already exists. Cannot add.`);
+          return; // Prevent adding if name exists
+        }
+        state.subGraphs.push({graphName, nodes, node_counter});
     },
     updateSubGraph: (state, action) => {
-        const { graphId, nodes, edges } = action.payload;
-         const existingGraphIndex = state.subGraphs.findIndex(graph => graph.graphId === graphId);
+        const { graphName, nodes, node_counter } = action.payload;
+         const existingGraphIndex = state.subGraphs.findIndex(graph => graph.graphName === graphName);
       
         if(existingGraphIndex !== -1){
             state.subGraphs[existingGraphIndex] = {
-              graphId: graphId,
+              graphName: graphName,
               nodes: nodes,
-              edges: edges
+              node_counter: node_counter,
             }
         } else {
             // If graph doesnt exist, add it
-            state.subGraphs.push({graphId: graphId, nodes: nodes, edges:edges})
+            state.subGraphs.push({graphName: graphName, nodes: nodes, node_counter: node_counter})
         }
     },
     removeSubGraph: (state, action) => {
-      state.subGraphs = state.subGraphs.filter(graph => graph.graphId !== action.payload);
+      state.subGraphs = state.subGraphs.filter(graph => graph.graphName !== action.payload);
     },
+    initSubGraphs: () => initialState, // Add reset reducer
   },
 });
 
-export const { setRootGraph, addSubGraph, updateSubGraph, removeSubGraph } = subGraphSlice.actions;
+export const { addSubGraph, updateSubGraph, removeSubGraph, initSubGraphs } = subGraphSlice.actions;
 export default subGraphSlice.reducer;
